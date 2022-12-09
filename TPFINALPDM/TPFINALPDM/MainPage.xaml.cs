3112eba1ffe5af419c5ab7f93157a858d7880542
Guilherme Uehara Piqui <uehara.piqui@gmail.com>
2022-12-09 20:14:39 -0300

    Initial commit

---
+++ TPFINALPDM/TPFINALPDM/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TPFINALPDM.Views;
+using Xamarin.Forms;
+
+namespace TPFINALPDM
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnCRUD_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CRUDMercadoria());
+        }
+
+        private async void BtnGeo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new GeoLocalizaPage());
+        }
+
+        private async void BtnCreditos_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Desenvolvedores", "Guilherme Uehara 1440209 \n Isabelle Gomes da Silva 3000249", "Ok");
+        }
+    }
+}
